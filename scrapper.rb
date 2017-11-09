46b9ceae3a290f8133051fc0563cd860d5188156
Diogo Batista <diogo.batista@ibood.com>
2017-11-09 08:54:43 +0100

    Added title detection

--- scrapper.rb
+++ scrapper.rb
@@ -3,5 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'open_uri_redirections'
    
-page = Nokogiri::HTML(open('http://github.com', :allow_redirections => :safe))   
-puts page   # => Nokogiri::HTML::Document
+url = 'https://www.amazon.com/gp/product/B01IEYHRAO'
+page = Nokogiri::HTML(open(url, :allow_redirections => :safe))   
+title =  page.at_css('[id="productTitle"]').text.strip   # => Nokogiri::HTML::Document
+puts title 
