074a65ef34c5f09305f3eb463accfe6e7a63eb9d
Diogo Batista <diogo.batista@ibood.com>
2017-11-09 09:40:46 +0100

    Added price getter

--- scrapper.rb
+++ scrapper.rb
@@ -5,5 +5,8 @@ require 'open_uri_redirections'
    
 url = 'https://www.amazon.com/gp/product/B01IEYHRAO'
 page = Nokogiri::HTML(open(url, :allow_redirections => :safe))   
-title =  page.at_css('[id="productTitle"]').text.strip   # => Nokogiri::HTML::Document
+title =  page.at_css('[id="productTitle"]').text.strip 
+price =  page.at_css('[id="priceblock_ourprice"]').text.strip # get only the first price
+
 puts title 
+puts price 
