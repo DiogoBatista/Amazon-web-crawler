90ec38120fbd83e984dd65809b83a70e439261d7
diogobatista <diogobv.batista@gmail.com>
2017-11-11 11:56:37 +0100

    Added currency and price trim

--- scrapper.rb
+++ scrapper.rb
@@ -6,7 +6,13 @@ require 'open_uri_redirections'
 url = 'https://www.amazon.com/gp/product/B01IEYHRAO'
 page = Nokogiri::HTML(open(url, :allow_redirections => :safe))   
 title =  page.at_css('[id="productTitle"]').text.strip 
-price =  page.at_css('[id="priceblock_ourprice"]').text.strip # get only the first price
+price =  page.at_css('[id="priceblock_ourprice"]').text.strip.split('-')[0]
 
-puts title 
-puts price 
+if price.include? "$"
+   currency = 'dolar'
+   price = price.tr('$', '')
+end
+
+puts title
+puts price
+puts currency
