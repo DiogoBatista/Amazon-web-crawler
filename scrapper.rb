9dd8336af8996084be4262684360974969206b1e
Miguel Nunes Correia <miguelncorreia@gmail.com>
2017-11-12 12:59:14 +0100

    big refactor

--- scrapper.rb
+++ scrapper.rb
@@ -7,185 +7,306 @@ require 'byebug'
 require 'net/http'
 require 'json'
 
-@results = { success: 0, failed: 0, error: [], repeated:0 }
+@@results = { success: 0, failed: 0, error: [], repeated: 0 }
+@robot_found = false
 
-def trimUrl(url)
-	url = url.to_s.strip
+USAGE = <<ENDUSAGE
+Usage:
+ruby scrapper.rb [--file PATH] [--api API] [--secret SECRET]
+ENDUSAGE
 
-	if url.include? "/ref="
-		url = url.gsub(/\/ref=.*/, '')
-	end
+HELP = <<ENDHELP
+-h, --help       Show this help.
+-f, --file       The input file with Amazon links, one per line
+-a, --api        The API endpoing where you want to do the POST request.
+ -s, --secret     Send this value as an Authorization Header. This is not a mandatory field.
+ENDHELP
 
-	if url.include? "?"
-		url = url.split('?')[0]
-	end
+ ARGS = {}
+ next_arg = ""
 
-	url
+ ARGV.each do |arg|
+  case arg
+  when '-h','--help'      then ARGS[:help] = true
+  when '-f','--file'     then next_arg = :file
+  when '-a','--api'    then next_arg = :api
+  when '-s','--secret'    then next_arg = :secret
+  else
+    if next_arg
+      ARGS[next_arg] = arg
+    end
+  end
 end
 
-def getImgUrlIfBase24(imgElement)
-	el = imgElement.css('img').attr('src')
-
-	if el.to_s.include? "data:image"
-		el = JSON.parse(imgElement.css("img").attr("data-a-dynamic-image").value).keys[0]
-	end
-
-	el.to_s
+if ARGS[:help]
+  puts USAGE
+  puts HELP
+  exit
 end
 
-def removeCurrencyFromPrice(price)
-	price = price.gsub(/[$£EUR ]/, '')
+if (!ARGS[:file] or !ARGS[:api])
+  puts USAGE
+  puts HELP
+  exit
 end
 
-def changeImgUrlToFormat(imgUrl, size)
-	format = "._SL#{size}_"
-	imgUrl = imgUrl.gsub(/\.\_S\w[0-9]+\_/, format)
+class Product
+  def initialize(url)
+    @url = clean_url(url)
+    @name = ""
+    @rating = ""
+    @image = ""
+    @price = 0.0
+    @currency = ""
+    @page = ""
+    @broken = false
+  end
+
+  def broken?
+    @broken
+  end
+
+  def valid?
+    not broken?
+  end
+
+  def params
+    {
+      name: @name,
+      photo: @image,
+      price: @price,
+      currency: @currency,
+      rating: @rating,
+      url: @url
+    }
+  end
+
+  def self.fetch(url)
+    new(url).fetch
+  end
+
+  def fetch
+    return if @url.to_s.empty?
+    puts ""
+    puts "Fetching #{@url}..."
+
+    @page = create_page
+    return false if broken? or robot_check?
+    
+    @name = scrape_name
+    return false if broken?
+
+    @price = scrape_price
+    return false if broken?
+
+    @rating = scrape_rating
+    return false if broken?
+
+    @image = scrape_image
+    return false if broken?
+
+    self
+  end
+
+  def post(api, secret = nil)
+    url = URI.parse(api)
+
+    headers = { 'Content-Type' => 'application/json' }
+    headers['Authorization'] = secret if secret
+
+    req = Net::HTTP::Post.new(url, headers)
+    req.body = params.to_json
+
+    res = Net::HTTP.start(url.hostname, url.port).request(req)
+
+    case res.code.to_i
+    when 201
+      puts "[201] Product #{@name} created."
+      @@results[:success] = @@results[:success] + 1 
+    when 409
+      puts "[409] Product #{@name} already existed."
+      @@results[:repeated] = @@results[:repeated] + 1
+    when 500
+      puts "[500] Some weird error on the backend"
+      puts "params: #{params}"
+      @@results[:failed] = @@results[:failed] + 1 
+      @@results[:error] << { code: res.code, url: @url }
+    else 
+      puts res.inspect
+      @@results[:failed] = @@results[:failed] + 1 
+      @@results[:error] << { code: res.code, url: @url }
+    end
+  end
+
+private
+
+  def clean_url(url)
+    url = url.to_s.strip
+
+    if url.include? "/ref="
+      url = url.gsub(/\/ref=.*/, '')
+    end
+
+    if url.include? "?"
+      url = url.split('?')[0]
+    end
+
+    url
+  end
+
+  def create_page
+    begin
+      @page = Nokogiri::HTML(open(@url, allow_redirections: :safe, "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"))
+    rescue OpenURI::HTTPError => e
+      code = e.io.status[0].to_i
+      @@results[:failed] = @@results[:failed] + 1 
+      @@results[:error] << { code: code, url: @url }
+
+      @broken = true
+      return puts "Page not found"
+    end
+  end
+
+  def scrape_name
+    name = ""
+
+    if name = @page.css('#productTitle, #title span')
+      name = name.text.strip
+    end
+
+    name = name.to_s
+
+    if name.empty?
+      @@results[:failed] = @@results[:failed] + 1 
+      @@results[:error] << { code: 'no-name', url: @url }
+
+      @broken = true
+      puts "No product name..."
+    end
+
+    name
+  end
+
+  def scrape_price
+    price = @page.at_css('#priceblock_ourprice, span.offer-price, #priceblock_saleprice, .a-color-price')
+
+    if price
+      price = price.text.strip.split('-')[0]
+
+      # Add currency before removing it from price
+      @currency = get_currency(price)
+      price = remove_currency_from_price(price)
+    end
+
+    price = price.to_f
+
+    unless price > 0.0
+      @@results[:failed] = @@results[:failed] + 1 
+      @@results[:error] << {code: 'no-name', url: @url }
+
+      @broken = true
+      puts "No product price..."
+    end
+
+    price
+  end
+
+  def remove_currency_from_price(price)
+    price.gsub(/[$£EUR ]/, '')
+  end
+
+  def get_currency(price)
+    if price.include? "EUR"
+      "€"
+    elsif price.include? "$"
+      "$"
+    elsif price.include? "£"
+      "£"
+    end
+  end
+
+  def scrape_rating
+    rating = @page.at_css('[id="averageCustomerReviews"]').css('i.a-icon-star').css("span.a-icon-alt").text
+
+    unless rating
+      @@results[:failed] = @@results[:failed] + 1 
+      @@results[:error] << {code: 'no-rating', url: @url }
+
+      @broken = true
+      puts "No product rating..." 
+    end
+
+    rating
+  end
+
+  def scrape_image
+    image = @page.at_css('#imgTagWrapperId img, #img-canvas img, #ebooks-img-canvas img')
+
+    unless image
+      @@results[:failed] = @@results[:failed] + 1 
+      @@results[:error] << { code: 'unavailable', url: @url }
+      
+      @broken = true
+      puts "No product image..." 
+    end
+
+    source = get_image_url(image)
+
+    convert_image_url_to_size(source, 400)
+  end
+
+  def get_image_url(image)
+    source = image.attr('src')
+
+    if source.include? "data:image"
+      source = JSON.parse(image.attr("data-a-dynamic-image")).keys[0]
+    end
+
+    source
+  end
+
+  def convert_image_url_to_size(source, size)
+    format = "._SL#{size}_"
+
+    source.gsub(/\.\_\w\w[0-9]+\_/, format)
+  end
+
+  def robot_check?
+    check = @page.to_s.include? "Sorry, we just need to make sure you're not a robot."
+    puts "Amazon is checking for robots... (ooops)" if check
+
+    check
+  end
 end
 
-def getCurrencyFromPrice(price)
-	if price.include? "EUR "
-		currency = '€'
-	elsif price.include? "$"
-		currency = '$'
-	else price.include? "£"
-		currency = '£'
-	end
+class Results
+  def self.all
+    @@results ||= { success: 0, failed: 0, error: [], repeated: 0 }
+  end
 end
 
-def sendProductToApi(apiHost, params, authorization)
-	apiUrl = URI.parse(apiHost + '/api/products')
+File.open(ARGS[:file]).readlines.each do |url|
+  next if @robot_found
+  next if url[0] == "#" 
+  next if url.strip == ""
 
-	req = Net::HTTP::Post.new(apiUrl, {
-		'Content-Type' => 'application/json',
-		'Authorization' => authorization
-		})
-	req.body = params.to_json
-	res = Net::HTTP.start(apiUrl.hostname, apiUrl.port).request(req)
-	
-	case res.code.to_i
-	when 201
-		@results[:success] = @results[:success] + 1 	
-	when 409
-		@results[:repeated] = @results[:repeated] + 1 	
-	else 
-		@results[:failed] = @results[:failed] + 1 	
-		@results[:error] << {code: res.code, url: params['url'] }
-	end
-	
-	puts res.inspect
+  product = Product.fetch(url)
+
+  product.post(ARGS[:api], ARGS[:secret])
+
+  # wait 5 seconds to avoid Amazon bot detection
+  sleep(5)
 end
 
-def createProductFromUrl(url,apiHost,secret)
-	url = trimUrl(url)
+results = @@results
 
-	puts ""
-	puts "Fetching '#{url}'..."
+puts ""
+puts "Successes: #{results[:success]}, Failed: #{results[:failed]}, repeated #{results[:repeated]}"
 
-	begin
-		page = Nokogiri::HTML(open(url, allow_redirections: :safe, "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36", "From" => "foo@bar.invalid", "Referer" => "http://www.ruby-lang.org/"))
-	rescue OpenURI::HTTPError => e
-		@results[:failed] = @results[:failed] + 1 	
-		@results[:error] << {code: e.io.status[0].to_i, url: url }
-		return puts "Page not found"
-	end
+if results[:error].length > 0 
+  puts ""
 
-	if name = page.at_css('[id="productTitle"]')
-		name = name.text.strip
-	elsif name = page.at_css('#title span')
-		name = name.text.strip
-	end
-
-	unless name
-		@results[:failed] = @results[:failed] + 1 	
-		@results[:error] << {code: 'no-name', url: url }
-		return puts "No product name..."
-	end
-
-	price = page.at_css('[id="priceblock_ourprice"]')
-
-	if price 
-		price = price.text.strip.split('-')[0]
-	elsif price = page.at_css('span.offer-price')
-		price = price.text
-	elsif price = page.at_css('#priceblock_saleprice')
-		price = price.text
-	elsif price = page.at_css('.a-color-price')
-		price = price.text
-	end
-	
-	unless price
-		@results[:failed] = @results[:failed] + 1 	
-		@results[:error] << {code: 'no-name', url: url }
-		return puts "No product price..."
-	end
-	
-	rating = page.at_css('[id="averageCustomerReviews"]').css('i.a-icon-star').css("span.a-icon-alt").text
-
-	unless rating
-		@results[:failed] = @results[:failed] + 1 	
-		@results[:error] << {code: 'no-rating', url: url }
-		return puts "No product rating..." 
-	end
-
-	if imgPageElement = page.at_css('[id="imgTagWrapperId"]')
-	elsif imgPageElement = page.at_css('#img-canvas')
-	elsif imgPageElement = page.at_css('#ebooks-img-canvas')
-	end
-
-	unless imgPageElement
-		@results[:failed] = @results[:failed] + 1 	
-		@results[:error] << {code: 'no-image', url: url }
-		return puts "No product image..." 
-	end
-	
-	imgUrl = getImgUrlIfBase24(imgPageElement)
-
-	currency = getCurrencyFromPrice(price)
-	price = removeCurrencyFromPrice(price)
-
-	imgUrl = changeImgUrlToFormat(imgUrl, 400)
-
-	params = {
-		'name' => name,
-		'photo' => imgUrl,
-		'price' => price,
-		'currency' => currency,
-		'rating' => rating,
-		'url' => url
-	}
-	puts params;
-
-	sendProductToApi(apiHost, params, secret)
-
+  results[:error].each do |e|
+    puts "#{e[:code]} >> #{e[:url]} "
+  end
 end
-
-
-arguments = ARGV.to_a
-
-if arguments.length != 3 
-	puts 'Not suficient arguments provided'
-else 
-	#todo validate arguments
-	file = arguments[0]
-	apiHost = arguments[1].to_s
-	secret = arguments[2].to_s	
-
-	File.open(file).readlines.each do |url|
-		next if url[0] == "#" 
-		next if url.strip == ""
-		createProductFromUrl(url,apiHost,secret)	
-	end
-
-	puts ""
-	puts "Successes: #{@results[:success]}, Failed: #{@results[:failed]}, repeated #{@results[:repeated]}"
-
-	if @results[:error].length > 0 
-		puts ""
-
-		@results[:error].each do |e|
-			puts "#{e[:code]} >> #{e[:url]} "
-		end
-	end
-end
-
-
